743edfc871c8b59aef9458e8d5c730dba996b9aa
Junqi Xie <junqi_xie@outlook.com>
2019-12-21 11:34:00 +0800

    Update realtime_detect_smiles.py
    Changed the file to use the SVC model saved from train_smile_detection_model.py.
    Note: users have to specify the model using "--model model_x.svc" explicitly.

--- realtime_detect_smiles.py
+++ realtime_detect_smiles.py
@@ -1,62 +1,67 @@
 import argparse
-import cv2,os,msvcrt
+import cv2
+import os
+import msvcrt
+import pickle
+import numpy as np
+import face_detection
+import train_smile_detection_model
 
-faceCascade = cv2.CascadeClassifier('xmls/haarcascade_frontalface_default.xml')
-def detect_face(in_img):
-    if in_img.ndim == 3:
-        gray = cv2.cvtColor(in_img, cv2.COLOR_BGR2GRAY)
-    elif in_img.ndim == 1:
-        gray = in_img
-    face_locations = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=8,
-        minSize=(55, 55),
-        flags=cv2.CASCADE_SCALE_IMAGE
-    )
-    return face_locations
-    
 
-smileCascade = cv2.CascadeClassifier('xmls/haarcascade_smile.xml')
-def detect_smile_face(in_img):    
-    if in_img.ndim == 3:
-        gray = cv2.cvtColor(in_img, cv2.COLOR_BGR2GRAY)
-    elif in_img.ndim == 1:
-        gray = in_img
-    smile_face_locations = smileCascade.detectMultiScale(
-            gray,
-            scaleFactor=1.16,
-            minNeighbors=35,
-            minSize=(25, 25),
-            flags=cv2.CASCADE_SCALE_IMAGE
-        )
-    return smile_face_locations
+def main(config):
+    try:
+        # real-time detecting smiles from computer's camera
+        with open(config.model, "rb") as fin:
+            svc = pickle.load(fin)  # load svc mode from the file saved
 
-def main():
-    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-    while True:            
-        res, frame = cap.read()
-        
-        if not res:
-            print('err reading camera.')
-            break
-            
-        draw_img = frame.copy()
-        face_locations = detect_face(frame)        
-        for (x, y, w, h) in face_locations:
-            cv2.rectangle(draw_img, (x, y), (x + w, y + h), (0, 225, 255), 2)
-            face = frame[y:y + h, x:x + w]
-            smile_face_locations = detect_smile_face(face)
-            if len(smile_face_locations) > 0:
-                cv2.rectangle(draw_img, (x, y), (x + w, y+ h), (0, 0, 255), 3)
-                cv2.putText(draw_img, 'Smiling', (x, y - 7), 3, 1.2, (0, 255, 0), 1, cv2.LINE_AA)
-        cv2.imshow('Real-Time Face Detect', draw_img)
-        cv2.waitKey(1)
-        if msvcrt.kbhit() and ord(msvcrt.getch()) == ord('q'):
-            break                
-    cap.release()
-    cv2.destroyAllWindows()
-        
+        cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)  # try to load the default camera of the PC
+        while True:
+            retval, frame = cap.read()  # retval: bool indicating whether we could find a camera
+                                        # frame: Array containing the picture captured
+            if not retval:
+                raise RuntimeError("cannot use the computer's camera")
 
-if __name__ == '__main__':
-    main()
+            draw_img = frame.copy()
+            face_locations = face_detection.detect_face(frame)
+            for (x, y, w, h) in face_locations:
+                cv2.rectangle(draw_img, (x, y), (x + w, y + h), (0, 225, 255), 2)  # use yellow rectangle to mark the face
+                face = frame[y:y + h, x:x + w]  # crop the face from the frame
+                gray_face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)  # convert face image to gray mode
+
+                if config.use_hog == False:
+                    feature = train_smile_detection_model.lbp(gray_face)  # get lbp feature
+                else:
+                    feature = train_smile_detection_model.hog_feature(gray_face)  # get hog feature
+                predict_result = svc.predict(np.array([feature]))  # use model to predict smiling
+
+                if predict_result == 1:
+                    cv2.rectangle(draw_img, (x, y), (x + w, y + h), (0, 0, 255), 3)  # use red rectangle to mark the smiling face
+                    cv2.putText(draw_img, "Smiling", (x, y - 7), 3, 1.2, (0, 255, 0), 1, cv2.LINE_AA)  # add a "Smiling" label
+
+            cv2.imshow("Real-Time Smile Detect", draw_img)  # show the image in a window
+            cv2.waitKey(1)  # refresh the frame per millisecond
+            if msvcrt.kbhit() and ord(msvcrt.getch()) == ord('q'):  # press 'q' to terminate the program
+                break
+
+        cap.release()  # release the camera resource
+        cv2.destroyAllWindows()  # destroy all the windows
+    except RuntimeError as e:
+        print("Error: {}".format(e.args))
+        return 1
+    except:
+        print("Error: unknown error")
+        raise
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    # usage:
+    # realtime_detect_smiles.py --model model_0.svc --use_hog False
+
+    parser.add_argument("--model", type = str, help = "the file containing the classifying model")
+    parser.add_argument("--use_hog", type = bool, default = False, help = "use hog feature instead of lbp")
+
+    config = parser.parse_args()
+
+    print(config)  # print out the configuration of the program in terminal
+    main(config)
