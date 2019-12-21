9d65837de2afd556aafdc1ec21eb2fc86047d308
Junqi Xie <junqi_xie@outlook.com>
2019-12-21 11:32:30 +0800

    Update train_smile_detection_model.py
    Added the support of HOG features to describe a face. To use it, try "--use_hog True".
    Changed some parameters to make the result more accurate.

--- train_smile_detection_model.py
+++ train_smile_detection_model.py
@@ -1,83 +1,149 @@
+import argparse
+import os
 import numpy as np
 from sklearn.model_selection import KFold
 from skimage.feature import local_binary_pattern
+from skimage.feature import hog
 from sklearn.svm import SVC
 from sklearn.metrics import f1_score, accuracy_score
 import cv2
+import pickle
 
-#step 1: get image names and labels
-print('loading image names and labels...')
-img_label_txt = 'img_label.txt'
-f = open(img_label_txt, 'r')
-lines = f.readlines()
-f.close()
 
-img_names = []
-labels = []
-for line in lines:
-    data = line.strip().split()
-    img_names.append(data[0])
-    labels.append(int(data[1]))
-labels = np.array(labels)
+def load_image(image_name):
+    in_img = cv2.imread(image_name)
+    if not os.path.exists(image_name):  # try to load the Input image specified in the parameters
+        raise RuntimeError("cannot find {}".format(image_name))
 
-#function: get lbp feature
-def lbp(image_name): 
-    '''
-    This function is provided by Mao&Yang&Zhao
-    input image_name: one color face image file name , str
-    output hist:  hist feature vecter, np.array
-    '''
-    image = cv2.imread(image_name)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    imagepart=[]
-    block=5
-    wideth=image.shape[1]
-    heigh=image.shape[0]
-    column = wideth // block
-    row=heigh//block
-    hist=np.array([])
-    for i in range(block*block):
-        lbp1 = local_binary_pattern(image[row*(i//block):row*((i//block)+1),column*(i % block):column*((i % block)+1)], 8, 1,  'default')
-        hist1, _ = np.histogram(lbp1, normed=True, bins=256, range=(0, 256))
-        hist=np.concatenate((hist,hist1))
+    if in_img.ndim == 3:  # Input image in RGB mode
+        return cv2.cvtColor(in_img, cv2.COLOR_BGR2GRAY)  # convert the Input image into gray mode. (As required by OpenCV)
+    elif in_img.ndim == 1:  # Input image in gray mode
+        return in_img[::]
+
+
+def lbp(image):
+    # get lbp feature
+    # This function is originally provided by Mao & Yang & Zhao, 
+    # modified and commented by Junqi Xie
+    # input image: one gray face image, np.array
+    # output hist: lbp feature vecter, np.array
+
+    # divide the image into (block * block) sections to deal with images of different resolutions
+    block = 6  # the number of blocks on each edge
+    width = image.shape[1]  # width of image
+    height = image.shape[0]  # height of image
+    column = width // block  # the number of pixels on a column
+    row = height // block  # the number of pixels on a row
+
+    hist = np.array([])
+    for i in range(block * block):
+        lbp1 = local_binary_pattern(image[row * (i // block):row * ((i // block) + 1), \
+            column * (i % block):column * ((i % block) + 1)], 8, 1, "default")  # get the lbp feature of a block
+        hist1, _ = np.histogram(lbp1, density = True, bins = 256, range = (0, 256))  # calculate the histogram of the feature
+        hist = np.concatenate((hist, hist1))  # concatenate the histogram with the previous ones
     return hist
 
-#step 2: get features
-print('computing image features...need a few minutes, please wait...')
-faces_path = 'data_faces/'
-features = lbp(faces_path + img_names[0])
-for i in range(1, len(img_names)):
-    features = np.vstack((features, lbp(faces_path + img_names[i])))
+
+def hog_feature(image):
+    # get hog feature
+    # input image: one gray face image, np.array
+    # output hist: hog feature vecter, np.array
+
+    # divide the image into (block * block) sections to deal with images of different resolutions
+    block = 7  # the number of blocks on each edge
+    width = image.shape[1]
+    height = image.shape[0]
+    column = width // block  # the number of pixels on a column
+    row = height // block  # the number of pixels on a row
+
+    hist = np.array([])
+    for i in range(block * block):
+        hist1 = hog(image[row * (i // block):row * ((i // block) + 1), column * (i % block):column * ((i % block) + 1)], \
+            pixels_per_cell = (column, row), cells_per_block = (1, 1))  # get the hog feature of a block (histogram)
+        hist = np.concatenate((hist, hist1))  # concatenate the histogram with the previous ones
+    return hist
 
 
-#step 3: training and testing, using 10-fold cross validation
-print('10-fold cross validation...')
-kf = KFold(n_splits=10, random_state=2019, shuffle=True)
-kf.get_n_splits(lines)
-for i, (train_index, test_index) in enumerate(kf.split(lines)):
-    features_train, features_test = features[train_index], features[test_index]
-    labels_train, labels_test = labels[train_index], labels[test_index]    
-    svc = SVC(kernel='linear', degree=2, gamma=1, coef0=0)
-    svc.fit(features_train, labels_train)
-    predict_result = svc.predict(features_test)
-    f1 = f1_score(labels_test, predict_result)
-    acc = accuracy_score(labels_test, predict_result)
-    print('fold %d, f1: %.5f, acc: %.5f, save predicted results in file %s' % (i, f1, acc, 'predicted_' + str(i) + '.txt'))
-    fout = open( 'predicted_' + str(i) + '.txt', 'w')
-    predict_cnt = 0
-    for j in test_index:
-        fout.write('%s %d %d\n' % (img_names[j], labels[j], predict_result[predict_cnt]))
-        predict_cnt = predict_cnt + 1
-    fout.close()
-    
-#your results should be like the following:
-#fold 0, f1: 0.86445, acc: 0.85791, save predicted results in file predicted_0.txt
-#fold 1, f1: 0.85354, acc: 0.84450, save predicted results in file predicted_1.txt
-#fold 2, f1: 0.87562, acc: 0.86595, save predicted results in file predicted_2.txt
-#fold 3, f1: 0.88462, acc: 0.87131, save predicted results in file predicted_3.txt
-#fold 4, f1: 0.87886, acc: 0.86290, save predicted results in file predicted_4.txt
-#fold 5, f1: 0.86730, acc: 0.84946, save predicted results in file predicted_5.txt
-#fold 6, f1: 0.88101, acc: 0.87366, save predicted results in file predicted_6.txt
-#fold 7, f1: 0.87229, acc: 0.85753, save predicted results in file predicted_7.txt
-#fold 8, f1: 0.86651, acc: 0.84677, save predicted results in file predicted_8.txt
-#fold 9, f1: 0.86375, acc: 0.85753, save predicted results in file predicted_9.txt
+def main(config):
+    try:
+        # step 1: get image names and labels
+        print("loading image names and labels...")
+        img_label = "{}.txt".format(config.img_label)
+        if not os.path.exists(img_label):  # try to load the file specified in the parameters
+            raise RuntimeError("cannot find {}".format(img_label))
+
+        img_names = []
+        labels = []
+        with open(img_label, "r") as f:  # read labels and convert it into np.array
+            lines = f.readlines()
+        for line in lines:
+            data = line.strip().split()
+            img_names.append(data[0])
+            labels.append(int(data[1]))
+        labels = np.array(labels)
+
+
+        # step 2: get features
+        print("computing image features... need a few minutes, please wait...")
+        faces_path = config.faces_path
+        if not os.path.exists("{}\\".format(faces_path)):  # try to load the folder specified in the parameters
+            raise RuntimeError("cannot find {}\\".format(faces_path))
+
+        if config.use_hog == False:
+            # use lbp to get features
+            features = lbp(load_image("{}\{}".format(faces_path, img_names[0])))
+            for i in range(1, len(img_names)):  # stack feature arrays in sequence vertically
+                features = np.vstack((features, lbp(load_image("{}\{}".format(faces_path, img_names[i])))))
+        else:
+            # use hog to get features
+            features = hog_feature(load_image("{}\{}".format(faces_path, img_names[0])))
+            for i in range(1, len(img_names)):  # stack feature arrays in sequence vertically
+                features = np.vstack((features, hog_feature(load_image("{}\{}".format(faces_path, img_names[i])))))
+
+
+        # step 3: training and testing, using 10-fold cross validation
+        print("10-fold cross validation...")
+        kf = KFold(n_splits = 10, random_state = 2019, shuffle = True)  # K-Folds cross-validator
+        kf.get_n_splits(lines)
+        for i, (train_index, test_index) in enumerate(kf.split(lines)):
+            features_train, features_test = features[train_index], features[test_index]  # get features
+            labels_train, labels_test = labels[train_index], labels[test_index]  # get labels
+
+            svc = SVC(kernel = "linear", degree = 2, gamma = 1, coef0 = 0)  # use SVM to classify
+            svc.fit(features_train, labels_train)  # trining data
+            with open("model_{}.svc".format(i), "wb") as fout:
+                pickle.dump(svc, fout)  # store the svc model to reuse later
+            predict_result = svc.predict(features_test)  # use testing data to get result
+            
+            f1 = f1_score(labels_test, predict_result)
+            acc = accuracy_score(labels_test, predict_result)
+
+            with open("predicted_{}.txt".format(i), "w") as fout:
+                predict_cnt = 0
+                for j in test_index:
+                    fout.write("{} {} {}\n".format(img_names[j], labels[j], predict_result[predict_cnt]))
+                    predict_cnt += 1
+
+            print("fold {}, f1: {:.5f}, acc: {:.5f}, save svc model in file model_{}.svc, \
+predicted results in file predicted_{}.txt".format(i, f1, acc, i, i))
+    except RuntimeError as e:
+        print("Error: {}".format(e.args))
+        return 1
+    except:
+        raise
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    # usage:
+    # train_smile_detection_model.py --faces_path data_faces --img_label img_label
+    # face_detection.py --use_hog True
+
+    parser.add_argument("--faces_path", type = str, default = "data_faces", help = "source input images path")
+    parser.add_argument("--img_label", type = str, default = "img_label", help = "text file containing labels of the images")
+    parser.add_argument("--use_hog", type = bool, default = False, help = "use hog feature instead of lbp")
+
+    config = parser.parse_args()
+
+    print(config)  # print out the configuration of the program in terminal
+    main(config)
